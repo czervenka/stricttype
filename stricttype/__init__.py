3c70c253daf272b3a18f13212002be738c773719
Robin Gottfried <czervenka@gmail.com>
2013-09-29 14:47:41 +0200

    dumps can now be importer right from stricttype package

--- stricttype/__init__.py
+++ stricttype/__init__.py
@@ -1,5 +1,5 @@
 VERSION = (0,0,1)
 
 from exceptions import *
-from message import Message, MessageType, MessageInstance
+from message import Message, MessageType, MessageInstance, dumps
 from base_types import *
